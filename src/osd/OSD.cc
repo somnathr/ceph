44a8c1d461a423c436f28ec1f4796cd75af31ca1
Greg Farnum <greg@inktank.com>
2014-03-20 10:05:14 -0700

    OSD: use safe params in map-sharing functions
    We were previously using unprotected access to OSD members.
    
    Unfortunately, this does not make them completely safe: we are looking up
    maps asynchronously from when we got access to the cached map bounds, and
    so the OSD could delete a map out from underneath us. Fixing that will
    require some kind of map bounds lock. :/
    
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -4627,7 +4627,7 @@ bool OSD::_share_map_incoming(
     if (sendmap) {
       dout(10) << name << " has old map " << epoch
 	       << " < " << osdmap->get_epoch() << dendl;
-      send_incremental_map(epoch, con);
+      send_incremental_map(epoch, con, osdmap);
       shared = true;
     }
   }
@@ -4646,7 +4646,7 @@ bool OSD::_share_map_incoming(
 	       << " has old map " << epoch << " < "
 	       << osdmap->get_epoch() << dendl;
       note_peer_epoch(name.num(), osdmap->get_epoch());
-      send_incremental_map(epoch, con);
+      send_incremental_map(epoch, con, osdmap);
       shared = true;
     }
   }
@@ -4666,7 +4666,7 @@ void OSD::_share_map_outgoing(int peer, Connection *con, OSDMapRef map)
   epoch_t pe = get_peer_epoch(peer);
   if (pe) {
     if (pe < map->get_epoch()) {
-      send_incremental_map(pe, con);
+      send_incremental_map(pe, con, map);
       note_peer_epoch(peer, map->get_epoch());
     } else
       dout(20) << "_share_map_outgoing " << con << " already has epoch " << pe << dendl;
@@ -5943,7 +5943,8 @@ void OSD::activate_map()
 }
 
 
-MOSDMap *OSD::build_incremental_map_msg(epoch_t since, epoch_t to)
+MOSDMap *OSD::build_incremental_map_msg(epoch_t since, epoch_t to,
+                                        OSDSuperblock& superblock)
 {
   MOSDMap *m = new MOSDMap(monc->get_fsid());
   m->oldest_map = superblock.oldest_map;
@@ -5974,7 +5975,8 @@ void OSD::send_map(MOSDMap *m, Connection *con)
   msgr->send_message(m, con);
 }
 
-void OSD::send_incremental_map(epoch_t since, Connection *con)
+void OSD::send_incremental_map(epoch_t since, Connection *con,
+                               OSDMapRef& osdmap)
 {
   epoch_t to = osdmap->get_epoch();
   dout(10) << "send_incremental_map " << since << " -> " << to
@@ -5999,7 +6001,7 @@ void OSD::send_incremental_map(epoch_t since, Connection *con)
 
   if (to - since > (epoch_t)cct->_conf->osd_map_message_max)
     to = since + cct->_conf->osd_map_message_max;
-  MOSDMap *m = build_incremental_map_msg(since, to);
+  MOSDMap *m = build_incremental_map_msg(since, to, superblock);
   send_map(m, con);
 }
 
