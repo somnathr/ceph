d1eded54955dcfc51611e64e8483c9c27437cc6b
Somnath Roy <somnath.roy@sandisk.com>
2014-04-24 17:59:52 -0700

    Fixed STM-79

--- src/os/LFNIndex.h
+++ src/os/LFNIndex.h
@@ -200,14 +200,14 @@ public:
   virtual int _split(
     uint32_t match,                             //< [in] value to match
     uint32_t bits,                              //< [in] bits to check
-    ceph::shared_ptr<CollectionIndex> dest  //< [in] destination index
+    CollectionIndex* dest  //< [in] destination index
     ) = 0;
   
   /// @see CollectionIndex
   int split(
     uint32_t match,
     uint32_t bits,
-    ceph::shared_ptr<CollectionIndex> dest
+    CollectionIndex* dest
     ) {
     WRAP_RETRY(
       r = _split(match, bits, dest);
